547643eebb7d04ca10f1ec488544dd3e2c05fef5
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2025-07-18 16:51:16 -0500

    Create Splash Screen

--- app/src/main/java/com/raulastete/spendless/MainActivity.kt
+++ app/src/main/java/com/raulastete/spendless/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.raulastete.spendless.screen.settings.preferences.PreferencesScreen
 import com.raulastete.spendless.ui.theme.SpendLessTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             SpendLessTheme {
